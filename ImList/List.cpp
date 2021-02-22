ca5b09ddca0b623c1a10a9cf0e9d8613fb4fe838
takkyu <mk.st.phys@gmail.com>
2021-02-23 06:40:26 +0900

    Imutable List structure

---
+++ ImList/List.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <ranges>
+#include <iterator>
+
+template <typename T>
+class ImList {
+public:
+    ImList() { }
+    ImList(std::initializer_list<T> il) {
+        auto newone = ImList();
+        for (auto elem : il | std::views::reverse)
+            newone = newone.cons(elem);
+        this->m_head = newone.m_head;
+    }
+    ImList empty() const { return ImList(); }
+    bool isEmpty() const { return m_head == nullptr; }
+    ImList cons(T newval) const {
+        if (isEmpty()) {
+            return ImList(std::make_shared<Node>(Node(newval)));
+        } else {
+            return ImList(std::make_shared<Node>(Node(newval, this->first())));
+        }
+    }
+    // return head, raise error if empty
+    T head() const { return this->first_if("head")->value; }
+    // return new ImList, raise error if empty
+    ImList tail() const { return ImList(this->first_if("tail")->tail); }
+    // concatenate two ImLists
+    ImList operator+(ImList rhs) const { return this->isEmpty() ? rhs : (this->tail() + rhs).cons(this->head()); }
+    ImList update(int i, T val) const {
+        if (i < 0) 
+            throw "Out of range: update";
+        else if (i == 0)
+            return tail().cons(val);
+        else
+            return this->tail().update(i-1, val).cons(this->head());
+    }
+private:
+    // Internal Node structure
+    struct Node {
+        Node(T val): value(val) { }
+        Node(T val, std::shared_ptr<Node> next): value(val), tail(next) { }
+        // Avoid recursion
+        ~Node() {
+            auto next_node = std::move(tail);
+            while (next_node) {
+                if (!next_node.unique()) break;
+                std::shared_ptr<Node> tail;
+                swap(tail, next_node->tail);
+                next_node.reset();
+                next_node = std::move(tail);
+            }
+        }
+        T value;
+        std::shared_ptr<Node> tail;
+    };
+    // Sentinel for the head
+    std::shared_ptr<Node> m_head;
+    ImList(std::shared_ptr<Node> head) : m_head(head) { }
+    // Get the pointer to the head
+    std::shared_ptr<Node> first() const { return m_head; }
+    // Get the pointer to the head, raise error if empty
+    std::shared_ptr<Node> first_if(std::string em) const { 
+        if (isEmpty())
+            throw "Error in range: " + em;
+        else
+            return m_head;
+    }
+};
+
+template <typename T>
+ImList<ImList<T>> suffixes(ImList<T> xs) {
+    return xs.isEmpty() ? ImList<ImList<T>>() : suffixes(xs.tail()).cons(xs);
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, ImList<T> xs) {
+    return xs.isEmpty() ? os << "()" : os << "(" << xs.head() << ":" << xs.tail() << ")";
+}
+
+int main() {
+    ImList<int> xs = ImList<int>({1,2,3,4});
+    std::cout << suffixes(xs) << std::endl;
+}

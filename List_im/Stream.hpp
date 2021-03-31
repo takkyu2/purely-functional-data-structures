3276f5faba49db691799aeef0ef9b93a3f5d0cb1
takkyu <mk.st.phys@gmail.com>
2021-03-31 14:51:22 +0900

    add make_thunk in stream

--- List_im/Stream.hpp
+++ List_im/Stream.hpp
@@ -64,29 +64,29 @@ public:
         return force().tail();
     }
     Stream operator+(Stream rhs) const {
-        return Stream(make_lazy_val([this_strm=*this, rhs_strm=rhs]() {
+        return make_thunk([this_strm=*this, rhs_strm=rhs]() {
                 if (this_strm.isEmpty()) return rhs_strm.force();
                 return StreamCell<Elem>(this_strm.head(), this_strm.tail() + rhs_strm);
-        }));
+        });
     }
     Stream take(int n) const {
-        return Stream(make_lazy_val([this_strm=*this, n=n]() {
+        return make_thunk([this_strm=*this, n=n]() {
                 if (n == 0 || this_strm.isEmpty()) return StreamCell<Elem>();
                 return StreamCell<Elem>(this_strm.head(), this_strm.tail().take(n-1));
-        }));
+        });
     }
     friend Stream cons(Elem e, const Stream& st) {
-        return Stream(make_lazy_val([elem=e, t=st]() {
+        return make_thunk([elem=e, t=st]() {
                 return StreamCell<Elem>(elem, t);
-        }));
+        });
     }
     Stream cons(Elem e) const {
         return cons(e, *this);
     }
     Stream drop(int n) const {
-        return Stream(make_lazy_val([this_strm=*this, n=n]() {
+        return make_thunk([this_strm=*this, n=n]() {
                 return this_strm.drop_helper(n).force();
-        }));
+        });
     }
     Stream drop_helper(int n) const {
         if (isEmpty()) return Stream();
@@ -97,33 +97,37 @@ public:
         return reverse();
     }
     Stream reverse() const {
-        return Stream(make_lazy_val([this_strm=*this]() {
+        return make_thunk([this_strm=*this]() {
                     return this_strm.reverse_helper({}).force();
-        }));
+        });
     }
     Stream reverse_helper(Stream r) const {
         if (isEmpty()) return r;
         return tail().reverse_helper(
-                Stream(make_lazy_val([head=head(), r=r]() {
+                make_thunk([head=head(), r=r]() {
                     return StreamCell<Elem>(head, r);
-            }))
+                })
         );
     }
     Stream sort() const {
-        return Stream(make_lazy_val([this_strm = *this]() {
+        return make_thunk([this_strm = *this]() {
                 if (this_strm.isEmpty()) return StreamCell<Elem>();
                 return this_strm.tail().sort().insert(this_strm.head()).force();
-        }));
+        });
     }
 private:
     Stream insert(Elem x) const { // For insertion sort on stream
-        return Stream(make_lazy_val([x=x, this_strm=*this]() {
+        return make_thunk([x=x, this_strm=*this]() {
                 if (this_strm.isEmpty()) return StreamCell<Elem>(x);
                 auto v = this_strm.head(); 
                 auto t = this_strm.tail(); 
                 if (x < v) return StreamCell<Elem>(x, this_strm); 
                 else return StreamCell<Elem>(v, t.insert(x));
-        }));
+        });
+    }
+    template <typename F>
+    static Stream make_thunk(F&& lam) {
+        return Stream(make_lazy_val(std::forward<F>(lam)));
     }
 };
 

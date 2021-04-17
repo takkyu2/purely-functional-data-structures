e3b79d2a8c7434799cd0790f4ec045649c9f48be
takkyu <mk.st.phys@gmail.com>
2021-04-17 14:33:22 +0900

    I found a workaround trick to avoid infinite polymorhic recursion :) (although not sure whether or not it is good practice...) We define the cutoff depth of the recursion, and if the number of elements is so large that the cutoff is reached, raise a runtime exception

--- RandomAccessList/AltBinaryRandomAccessList.cpp
+++ RandomAccessList/AltBinaryRandomAccessList.cpp
@@ -3,15 +3,13 @@
 
 int main() {
     AltBinaryRandomAccessList<int> vec;
-    std::cout << vec.isEmpty() << std::endl;
-    auto vec2 = vec.cons(0);
-    for (int i = 0; i < 1; ++i) {
-        /* vec = vec.cons(i); */
+    for (int i = 0; i < 100; ++i) {
+        vec = vec.cons(i);
         /* vec = vec.cons(i); */
     }
-    /* for (int i = 0; i < 3; ++i) { */
-    /*     /1* std::cout << vec.head() << std::endl; *1/ */
-    /*     std::cout << vec.lookup(i) << std::endl; */
-    /*     /1* vec = vec.tail(); *1/ */
-    /* } */
+    for (int i = 0; i < 100; ++i) {
+        /* std::cout << vec.head() << std::endl; */
+        std::cout << vec.lookup(i) << std::endl;
+        /* vec = vec.tail(); */
+    }
 }
